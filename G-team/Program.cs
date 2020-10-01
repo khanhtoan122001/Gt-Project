23951a324c8ec5bdca2df86995f64e54be7dcfa2
yamianhly <hoanganh18346@gmail.com>
2020-10-01 19:15:04 +0700

    Update Function

--- G-team/Program.cs
+++ G-team/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Fcn;
 namespace G_team
 {
     static class Program
@@ -14,9 +14,14 @@ namespace G_team
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            Bac_n a = new Bac_n();
+            float[] b = { 2, 1 };
+            a.X = b;
+            a.N = 2;
+            Console.WriteLine(a.f(2));
         }
     }
 }
