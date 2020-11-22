bce0a26ec94bbc77c247091e35c1f9aacb1717f9
khanhtoan122001 <71191923+khanhtoan122001@users.noreply.github.com>
2020-11-23 00:10:40 +0700

    Revert "no message"
    This reverts commit 422d4fff2845d4106dda8b0c7c9ae3c09bd76c5e.

--- GT/GT/Form1.Designer.cs
+++ GT/GT/Form1.Designer.cs
@@ -323,7 +323,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
             this.ClientSize = new System.Drawing.Size(1187, 612);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
